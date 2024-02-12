0aa6ccd692333d8e8e843440b0b894eeae6f414b
Boddiolla12 <Oskar.gjelstad@gmail.com>
2024-02-12 22:30:47 +0100

    Ferdig med oppgave 2

--- assignments/assignment_02.js
+++ assignments/assignment_02.js
@@ -1,3 +1,17 @@
 export function findLongestName(countries) {
-    // Skriv koden her
+  // Skriv koden her
+  const input = ["Sweeden", "Denmark", "Norway", "Switzerland", "Iceland", "England"];
+
+  if (input.length === 0) {
+    return null;
+  }
+  let longestWord = input[0];
+
+  for (let i = 1; i < input.length; i++) {
+    if (input[i].length > longestWord.length) {
+      longestWord = input[i];
+    }
+  }
+
+  return longestWord;
 }
