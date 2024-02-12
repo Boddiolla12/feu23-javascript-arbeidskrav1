bff6134c6d88200f29070c83fc98ff78be8bba6d
Boddiolla12 <Oskar.gjelstad@gmail.com>
2024-02-12 22:12:29 +0100

    Gjort ferdig oppgave 1

--- assignments/assignment_01.js
+++ assignments/assignment_01.js
@@ -1,3 +1,11 @@
 export function convertInitialLettersToUpperCase(input) {
-    // Skriv koden her
+  let inputText = "Aldri har så mange hatt så få å takke for så mye";
+
+  let ordArray = inputText.split(" ");
+
+  let storBokstavArray = ordArray.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+
+  let storBokstavString = storBokstavArray.join(" ");
+
+  return storBokstavString;
 }
