16ee6f73badb7bcd88656bdaf88f32fb0268f705
Boddiolla12 <Oskar.gjelstad@gmail.com>
2024-02-13 20:41:26 +0100

    Edit oppgave 7

--- __tests__/assignment_07.test.js
+++ __tests__/assignment_07.test.js
@@ -2,29 +2,29 @@ import { expect } from "@jest/globals";
 import { decrypt, encrypt } from "../assignments/assignment_07";
 
 describe("Oppgave 07", () => {
-    it("Krypter melding med Cease cypher", () => {
-        // arrange
-        const message = "Experience is the teacher of all things.";
+  it("Krypter melding med Cease cypher", () => {
+    // arrange
+    const message = "Experience is the teacher of all things.";
 
-        const expected = "Lbwlyplujl pz æol ælhjoly vm hss æopunz.";
+    const expected = "Lbwlyplujl pz æol ælhjoly vm hss æopunz.";
 
-        // act
-        const result = encrypt(message, 7);
+    // act
+    const result = encrypt(message, 7);
 
-        // assert
-        expect(result).toEqual(expected);
-    });
+    // assert
+    expect(result).toEqual(expected);
+  });
+});
 
-    it.skip("Dekrypter melding med Cease cypher", () => {
-        // arrange
-        const message = "Mhj hu hq Dzhvrph surjudphuhu!";
+it.skip("Dekrypter melding med Cease cypher", () => {
+  // arrange
+  const message = "Mhj hu hq Dzhvrph surjudphuhu!";
 
-        const expected = "Jeg er en Awesome programerer!";
+  const expected = "Jeg er en Awesome programerer!";
 
-        // act
-        const result = decrypt(message);
+  // act
+  const result = decrypt(message);
 
-        // assert
-        expect(result).toEqual(expected);
-    });
+  // assert
+  expect(result).toEqual(expected);
 });
--- assignments/assignment_07.js
+++ assignments/assignment_07.js
@@ -1,7 +1,46 @@
 export function encrypt(message, key) {
-    // Skriv koden her
+  let result = "";
+
+  const firstUpperCase = message.toUpperCase().charCodeAt(0);
+  const firstLowerCase = message.toLowerCase().charCodeAt(0);
+
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+
+    if (char.match(/[a-zæøå]/i)) {
+      const code = message.charCodeAt(i);
+
+      if (char === char.toUpperCase()) {
+        char = String.fromCharCode(((code - firstUpperCase + key) % 26) + firstUpperCase);
+      } else {
+        char = String.fromCharCode(((code - firstLowerCase + key) % 26) + firstLowerCase);
+      }
+    }
+
+    result += char;
+  }
+  return result;
 }
 
-export function decrypt(cipher, key) {
-    // Skriv koden her
+export function decrypt(message, key) {
+  let result = "";
+
+  const firstUpperCase = message.toUpperCase().charCodeAt(0);
+  const firstLowerCase = message.toLowerCase().charCodeAt(0);
+
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+
+    if (char.match(/[a-z]/i)) {
+      let code = message.charCodeAt(i);
+
+      if (char === char.toUpperCase()) {
+        char = String.fromCharCode(((code - firstUpperCase - key + 26) % 26) + firstUpperCase);
+      } else {
+        char = String.fromCharCode(((code - firstLowerCase - key + 26) % 26) + firstLowerCase);
+      }
+    }
+    result += char;
+  }
+  return result;
 }
